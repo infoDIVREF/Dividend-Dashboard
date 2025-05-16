c57ed71487a6cacd0a9c869a80b67fb642e69a34
ATTOMO DIGITAL <87724533+ATTOMODIGITAL@users.noreply.github.com>
2025-05-16 14:46:30 +0200

    includo flag types

--- src/app/components/DocumentsPage/DocumentsDashboard/DocumentsDashboard.jsx
+++ src/app/components/DocumentsPage/DocumentsDashboard/DocumentsDashboard.jsx
@@ -43,8 +43,6 @@ export default function DocumentsDashboard() {
     fetchDocumentsData();
   }, [token, collaboratorId]);
 
-  console.log("Documents data:", documents);
-
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
