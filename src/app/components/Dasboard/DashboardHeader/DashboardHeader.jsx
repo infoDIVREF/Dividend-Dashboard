32821bda53e8c1de068aa31cbcee6012eb8ec554
ATTOMO DIGITAL <msiesto.garabana.dev@gmail.com>
2025-06-18 12:11:43 +0200

    fix: dashboardheader pointer events

--- src/app/components/Dasboard/DashboardHeader/DashboardHeader.jsx
+++ src/app/components/Dasboard/DashboardHeader/DashboardHeader.jsx
@@ -8,7 +8,7 @@ export default function DashboardHeader({
   isSidebarOpen,
 }) {
   return (
-    <div className={`w-[80vw] flex flex-row justify-between absolute right-0 items-center h-40 pt-[67px] px-[54px] z-30 ${pageToShow === "map" ? "": "bg-blanco"}`}>
+    <div className={`w-[80vw] flex flex-row justify-between absolute right-0 items-center h-40 pt-[67px] px-[54px] z-30 ${pageToShow === "map" ? "" : "bg-blanco"}`} style={{ pointerEvents: 'none' }}>
       <div className={`flex ${!isSidebarOpen ? "gap-5" : ""}`}>
         <div className="flex items-center justify-center">
           {!isSidebarOpen && pageToShow !== "map" ? (
@@ -24,20 +24,22 @@ export default function DashboardHeader({
       <div className="flex gap-4">
         <button
           onClick={() => setPageToShow("graphics")}
+          style={{ pointerEvents: 'auto' }}
           className={`flex items-center justify-center gap-2 px-4 py-2 rounded-md transition-all ease-in-out duration-300 w-32 min-w-32 font-bricolage ${
             pageToShow === "graphics"
-              ? "bg-blanco-roto text-negro" 
-              : "bg-transparent border border-gris-claro " 
+              ? "bg-blanco-roto text-negro"
+              : "bg-transparent border border-gris-claro"
           }`}
         >
           Gráficos
         </button>
         <button
           onClick={() => setPageToShow("map")}
+          style={{ pointerEvents: 'auto' }}
           className={`flex items-center justify-center gap-2 px-4 py-2 rounded-md transition-all ease-in-out duration-300 w-32 min-w-32 font-bricolage ${
             pageToShow === "map"
-              ? "bg-blanco-roto text-negro" 
-              : "bg-transparent border border-gris-claro" 
+              ? "bg-blanco-roto text-negro"
+              : "bg-transparent border border-gris-claro"
           }`}
         >
           Mapa
