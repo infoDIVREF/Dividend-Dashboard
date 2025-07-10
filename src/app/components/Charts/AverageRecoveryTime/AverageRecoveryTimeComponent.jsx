8a0e97c570ec5d027edc9e381225b5705c1793ba
ATTOMO DIGITAL <msiesto.garabana.dev@gmail.com>
2025-07-10 11:38:52 +0200

    feat: change font-sizes

--- src/app/components/Charts/AverageRecoveryTime/AverageRecoveryTimeComponent.jsx
+++ src/app/components/Charts/AverageRecoveryTime/AverageRecoveryTimeComponent.jsx
@@ -10,79 +10,6 @@ export function AverageRecoveryTimeComponent() {
 
   const { data, loading, error } = useGetAverageRecoveryTime();
 
-  const mockData = [
-    {
-      country: "DE",
-      method: "TJUE",
-      averageRecoveryTime: 0,
-      minimumRecoveryTime: 0,
-      maximumRecoveryTime: 0,
-    },
-    {
-      country: "BE",
-      method: "DTTR",
-      averageRecoveryTime: 0,
-      minimumRecoveryTime: 0,
-      maximumRecoveryTime: 0,
-    },
-    {
-      country: "CA",
-      method: "TJUE",
-      averageRecoveryTime: 13,
-      minimumRecoveryTime: 9,
-      maximumRecoveryTime: 17,
-    },
-    {
-      country: "IE",
-      method: "DTTR",
-      averageRecoveryTime: 12,
-      minimumRecoveryTime: 7,
-      maximumRecoveryTime: 16,
-    },
-    {
-      country: "IT",
-      method: "TJUE",
-      averageRecoveryTime: 16,
-      minimumRecoveryTime: 11,
-      maximumRecoveryTime: 21,
-    },
-    {
-      country: "NO",
-      method: "DTTR",
-      averageRecoveryTime: 18,
-      minimumRecoveryTime: 12,
-      maximumRecoveryTime: 22,
-    },
-    {
-      country: "PL",
-      method: "TJUE",
-      averageRecoveryTime: 10,
-      minimumRecoveryTime: 6,
-      maximumRecoveryTime: 15,
-    },
-    {
-      country: "PT",
-      method: "DTTR",
-      averageRecoveryTime: 11,
-      minimumRecoveryTime: 7,
-      maximumRecoveryTime: 14,
-    },
-    {
-      country: "SE",
-      method: "TJUE",
-      averageRecoveryTime: 12,
-      minimumRecoveryTime: 8,
-      maximumRecoveryTime: 17,
-    },
-    {
-      country: "CH",
-      method: "DTTR",
-      averageRecoveryTime: 14,
-      minimumRecoveryTime: 9,
-      maximumRecoveryTime: 19,
-    },
-  ];
-
   // Filter the mockData to keep only countries that are present in selectedCountries
   const selectedCountryCodes = new Set(
     selectedCountries.map((country) => country.isoCode)
@@ -92,10 +19,6 @@ export function AverageRecoveryTimeComponent() {
     selectedCountryCodes.has(item.country)
   );
 
-  /* const filteredData = mockData.filter((item) =>
-    selectedCountryCodes.has(item.country)
-  ); */
-
   if (loading) return <p>Cargando datos...</p>;
   if (error || !data) return <p>Hubo un error al cargar los datos.</p>;
   return (
@@ -104,7 +27,7 @@ export function AverageRecoveryTimeComponent() {
         <h3 className="leading-7 text-[1rem]  font-semibold">
           Tiempos de recuperación
         </h3>
-        <div className="flex gap-5 text-[17px]">
+        <div className="flex gap-5 text-[12px]">
           <div className="flex items-center gap-2">
             <div className="bg-[#3fb1f8] rounded-2xl w-10 h-6" />
             <span>Mínimos</span>
