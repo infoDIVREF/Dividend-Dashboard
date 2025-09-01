280fe288ea2177b78a6c65977a136cc1fd60806e
ATTOMO DIGITAL <msiesto.garabana.dev@gmail.com>
2025-09-01 16:00:39 +0200

    missing hover

--- src/app/components/SideBar/ToggleFiltersComponent/ToggleDashboardComponent.tsx
+++ src/app/components/SideBar/ToggleFiltersComponent/ToggleDashboardComponent.tsx
@@ -8,7 +8,7 @@ export default function ToggleDashboardComponent({
   return (
     <button
       onClick={toggleSidebar}
-      className="pointer-events-auto flex items-center justify-center gap-2 px-2 py-2 rounded-[15px] transition-all ease-in-out duration-300 w-32 font-bricolage bg-blanco-roto text-negro"
+      className="hover-reduce-opacity pointer-events-auto flex items-center justify-center gap-2 px-2 py-2 rounded-[15px] transition-all ease-in-out duration-300 w-32 font-bricolage bg-blanco-roto text-negro"
     >
       Filtros
       <ChevronsRight size={20} strokeWidth={1.5} />
