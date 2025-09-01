9d7fd026501e503864d1bc83183d6add7024f18b
ATTOMO DIGITAL <msiesto.garabana.dev@gmail.com>
2025-09-01 15:13:39 +0200

    feat: text is invoices is empty

--- src/app/components/Charts/YearsChart/YearsChart.jsx
+++ src/app/components/Charts/YearsChart/YearsChart.jsx
@@ -8,7 +8,6 @@ import {
   Tooltip,
   ResponsiveContainer,
   Legend,
-  CartesianGrid,
 } from "recharts";
 import { useGetDataByYear } from "@/hooks/useGetDataByYear";
 import { CustomLegend } from "../CustomLegend";
